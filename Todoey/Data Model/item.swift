87d4d2e04703a316cf0b2a6f5e14d6709b27a1c1
Kyle Garcia <kylegarcia88@gmail.com>
2019-02-13 16:52:48 -0800

    convert to mvc design pattern

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,15 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Kyle Garcia on 2/13/19.
+//  Copyright © 2019 Kyle Garcia. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
